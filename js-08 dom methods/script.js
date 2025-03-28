8594b7064df0e7a4853f3cb190b73afbe62a25a1
mrparth1712 <parthbajariya@gmail.com>
2025-03-28 18:53:24 +0530

    new add

---
+++ js-08 dom methods/script.js
@@ -0,0 +1,8 @@
+document.getElementsByTagName('img')
+let firsts=document.getElementsByClassName('firsts');
+let cass=document.getElementById('css');
+let first=document.querySelector('.first');
+let ols=document.querySelector('.ols');
+let pq=document.getElementById('#pq');
+pq.innerHTML="<u>HTML</ul>";
+console.log(pq);
